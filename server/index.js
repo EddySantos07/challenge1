75610f3844611e2c9155ac9903ddf519aa8f958e
Eddy <eagle100065@gmail.com>
2022-04-04 06:45:51 -0400

    added backend to filter out if any choices are filled out to calculus

--- server/index.js
+++ server/index.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
-app.use(express.static('public'))
+let bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + "/../public"));
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.post("/mainForm", (req, res) => {
+  console.log(req.body, "bODY");
+
+  let count = 0;
+
+  for (let i = 0; i < req.body.length; i++) {
+    if (req.body[i] === "") continue;
+
+    if (req.body[i] === "calculus") {
+      count++;
+    }
+
+    if (count > 0) {
+      res.sendStatus(200);
+    }
+
+    res.sendStatus(400);
+  }
+
+  res.send("good");
 });
 
 app.listen(port, () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 
+import axios from "axios";
+
 function App() {
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
     const data = new FormData(event.target);
 
-    console.log(data.get("choices[]1"));
+    let choice1 = data.get("choices[]1");
+    let choice2 = data.get("choices[]2");
+    let choice3 = data.get("choices[]3");
+
+    let choices = [choice1, choice2, choice3];
+
+    let calculusCount = 0;
+
+    for (let i = 0; i < choices.length; i++) {
+      if (choices[i] === "") continue;
+
+      if (choices[i].toLowerCase() === "calculus") {
+        calculusCount++;
+      }
+    }
+
+    if (calculusCount > 0) {
+      axios
+        .post("/mainForm", {
+          choices,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
   };
 
   return (
